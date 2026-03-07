643dd61640e0d41837693813f489b3cba472b747
castella <carissa2206@gmail.com>
2026-03-07 17:02:08 +0000

    Jobs view

---
+++ wit26/Cards/TradeswomenCardView.swift
@@ -0,0 +1,18 @@
+//
+//  TradeswomenCardView.swift
+//  wit26
+//
+//  Created by totoro tabby on 07/03/2026.
+//
+
+import SwiftUI
+
+struct TradeswomenCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TradeswomenCardView()
+}
--- wit26/Views/JobsView.swift
+++ wit26/Views/JobsView.swift
@@ -8,8 +8,71 @@
 import SwiftUI
 
 struct JobsView: View {
+    
+    @State var searchText = ""
+    @State private var selectedTab = 0
+    let items = ["Painter/Decorator", "Electrician", "Plumber", "Gardener", "Mechanic"]
+    let icons = [
+        "paintbrush.fill",
+        "lightbulb.fill",
+        "drop.fill",
+        "leaf.fill",
+        "wrench.and.screwdriver.fill"
+    ]
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        CapyTextField(title: "Search", text: $searchText, systemIcon: "magnifyingglass")
+        Picker("Tabs", selection: $selectedTab) {
+                Text("Jobs").tag(0)
+                Text("Errands").tag(1)
+            }
+            .pickerStyle(.segmented)
+            .padding()
+                    
+        if selectedTab == 0 {
+            VStack {
+                NavigationStack{
+                    List {
+                        ForEach(items.indices, id: \.self) { index in
+                            NavigationLink(destination: DetailView(job: items[index])) {
+                                HStack {
+                                    Image(systemName: icons[index])
+                                        .frame(width: 30)
+                                        .foregroundColor(.blue)
+                                    
+                                    Text(items[index])
+                                }
+                            }
+                        }
+                    }
+                    .listStyle(.plain)
+                }
+            }
+            }
+    }
+}
+
+struct DetailView: View {
+    let job: String
+
+    var body: some View {
+        VStack {
+            switch job {
+            case "Painter/Decorator":
+                TradeswomenCardView()
+            case "Electrician":
+                Text("Electrician details here")
+            case "Plumber":
+                Text("Plumber details here")
+            case "Gardener":
+                Text("Gardener details here")
+            case "Mechanic":
+                Text("Mechanic details here")
+            default:
+                Text("Unknown job")
+            }
+            Spacer()
+        }
+        .navigationTitle(job)
     }
 }
 
